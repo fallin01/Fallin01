3e6539f8ae224dd045c9044b9dbd3b552cc98200
fallin01 <46939349+fallin01@users.noreply.github.com>
2019-03-17 01:21:43 -0500

    homework4

---
+++ Gamming HW 4/NavMesh Example Project/Assets/clock.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class clock : MonoBehaviour
+{
+    public Text currentMinutes;
+    public Text currentSeconds;
+    public Text totalMinutes;
+    public Text totalSeconds;
+}
