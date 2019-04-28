2caebce28774f182c2f712f1aa360bd55039ccec
fallin01 <46939349+fallin01@users.noreply.github.com>
2019-04-28 12:28:45 -0500

    Fruit ninja

---
+++ Fruit-Ninja-Replica-master/Fruit Ninja Replica/Assets/Scripts/SceneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+   public void dropdownNext()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Quit()
+    {
+        SceneManager.LoadScene("EndScreen");
+    }
+    public void replay()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void backtostart()
+    {
+        SceneManager.LoadScene("DropDown");
+    }
+    public void endgame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+    public void scoreScreen()
+    {
+        SceneManager.LoadScene("LastScreen");
+    }
+}
